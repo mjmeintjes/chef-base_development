7b1c691573ea6a0f3da7326799cafe89c6e8455f
Matt Meintjes <matthys@mattsum.com>
2014-01-02 22:51:03 +1300

    added mattsum user creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,41 @@
-template "/home/vagrant/.bashrc" do
+user 'mattsum' do
+  group 'mattsum'
+  system true
+  shell "/bin/bash"
+  home "/home/mattsum"
+  password "$1$9teDIHue$310PmfM27Dp.1TkLlSstN0"
+  supports :manage_home => true
+end
+
+group 'sudo' do
+  append true
+  members 'mattsum'
+  action :modify
+end
+
+template "/home/mattsum/.bashrc" do
   source "bashrc.erb"
   mode 0440
-  owner "vagrant"
-  group "vagrant"
+  owner "mattsum"
+  group "mattsum"
+end
+
+directory "/home/mattsum/.ssh" do
+  recursive true
+  owner 'mattsum'
+  group 'mattsum'
+end
+
+cookbook_file "/home/mattsum/.ssh/authorized_keys" do
+  source "authorized_keys"
+  owner 'mattsum'
+  group 'mattsum'
+  mode "0600"
+end
+
+cookbook_file "/home/mattsum/.ssh/authorized_keys2" do
+  source "authorized_keys"
+  owner 'mattsum'
+  group 'mattsum'
+  mode "0600"
 end
