f089f841c23528f48ad6de58408864b74da33955
Matt Meintjes <matthys@mattsum.com>
2014-01-03 14:13:17 +1300

    updated pip_package to pip

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Matt Meintjes'
 maintainer_email 'matt@mattsum.com'
 license          'All rights reserved'
 description      'Sets up generic development environment'
-version          '0.0.5'
+version          '0.0.6'
 
 depends "build-essential"
 depends "vim"
