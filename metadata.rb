3a0b8d6f2ed7b4aca2bd427399f00d242a0a74cd
Matt Meintjes <matthys@mattsum.com>
2014-01-03 16:04:07 +1300

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Matt Meintjes'
 maintainer_email 'matt@mattsum.com'
 license          'All rights reserved'
 description      'Sets up generic development environment'
-version          '0.0.10'
+version          '0.0.11'
 
 depends "build-essential"
 depends "vim"
